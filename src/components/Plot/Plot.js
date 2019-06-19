156809ecb29abdeb3566bfb0107eae34ab1b6b4c
Wojciech Musial <wojciechmusial23@gmail.com>
2019-06-20 01:05:09 +0200

    added plotly

--- src/components/Plot/Plot.js
+++ src/components/Plot/Plot.js
@@ -1,14 +1,37 @@
 import React, { Component } from 'react';
 import './Plot.css';
+import Plotly from 'react-plotly.js';
 
 class Plot extends Component {
 
     render() {
-        return(
+        return (
             <div className="Plot">
                 <p>Plot component works!</p>
+                <Plotly
+                    data={[
+                        {
+                            type: 'scatter',
+                            x: [1, 2, 3],
+                            y: [2, 6, 3],
+                            mode: 'lines+points',
+                            marker: {color: 'red'},
+                        },
+                        {
+                            type: 'bar',
+                            x: [1, 2, 3],
+                            y: [2, 1, 3]
+                        },
+                    ]}
+                    layout={{
+                        title: 'Plot title',
+                        autosize: true
+                    }}
+                    style={{ width: '100%', height: '100%' }}
+                    useResizeHandler
+                />
             </div>
-        )
+        );
     }
 }
 
