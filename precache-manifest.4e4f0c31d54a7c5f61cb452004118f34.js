ef22841f764fdd5dc3862137ae67bf5c364e9f4d
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2021-03-08 06:18:37 +0000

    Deploy APTG/dedx_web to github.com/APTG/dedx_web.git:gh-pages

---
+++ precache-manifest.4e4f0c31d54a7c5f61cb452004118f34.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0d347185e1dd663022d523a2fe84e502",
+    "url": "/dedx_web/index.html"
+  },
+  {
+    "revision": "d265d09baf3f8e200ca8",
+    "url": "/dedx_web/static/css/2.48db8b0f.chunk.css"
+  },
+  {
+    "revision": "e4bf0e30371a226263c4",
+    "url": "/dedx_web/static/css/main.06fc1afc.chunk.css"
+  },
+  {
+    "revision": "d265d09baf3f8e200ca8",
+    "url": "/dedx_web/static/js/2.a6e21bef.chunk.js"
+  },
+  {
+    "revision": "e4bf0e30371a226263c4",
+    "url": "/dedx_web/static/js/main.07eb1f90.chunk.js"
+  },
+  {
+    "revision": "162ec073785f8d628e38",
+    "url": "/dedx_web/static/js/runtime-main.9933ecf3.js"
+  }
+]);
