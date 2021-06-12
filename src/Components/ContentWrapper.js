b9cea8be98f9a6d1007e084b25ea7cc1ff2e769e
Mexolius <39342674+Mexolius@users.noreply.github.com>
2021-06-12 14:49:56 +0200

    Integrating component into ContentWrapper

--- src/Components/ContentWrapper.js
+++ src/Components/ContentWrapper.js
@@ -1,42 +1,53 @@
 import { Component } from "react";
 import makeAsyncScriptLoader from "react-async-script";
+import JSRootGraph from "./JSRootGraph";
 
 
 
-const JSRootLink ='https://root.cern.ch/js/latest/scripts/JSRoot.core.js';
+const JSRootLink = 'https://root.cern.ch/js/latest/scripts/JSRoot.core.js';
 
-class ContentWrapper extends Component{
-    constructor(props){
+class ContentWrapper extends Component {
+    constructor(props) {
         super(props);
 
         this.state = {
-            ready:false,
-            traces:[]
+            ready: false,
+            traces: []
         }
 
         this.submithandler = this.submitHandler.bind(this);
     }
 
-    shouldComponentUpdate(_){
+    shouldComponentUpdate(_) {
         return !this.props.JSROOT
     }
 
+
+    static getDerivedStateFromProps(props, _) {
+        if (props.JSROOT) {
+            console.log("ready");
+            return {
+                ready: true
+            }
+        }
+        return null;
+
+    }
+
     //TODO
-    submitHandler(){
-        this.setState(state=>({
+    submitHandler() {
+        this.setState(state => ({
             traces: state.traces.append([]/*Incomming data*/)
         }))
     }
 
-    render(){
-        if(this.props.JSROOT) console.log("ready")
-        return(
+    render() {
+        return (
             <div className="content">
                 {
-                    /*
-                    <Form/>
-                    <Graph/>
-                    */
+                    this.state.ready
+                        ? <JSRootGraph traces = {this.state.traces} />
+                        : <div>JSROOT still loading</div>
                 }
             </div>
         )
@@ -45,6 +56,6 @@ class ContentWrapper extends Component{
 
 
 
-export default makeAsyncScriptLoader(JSRootLink,{
-    globalName:"JSROOT"
+export default makeAsyncScriptLoader(JSRootLink, {
+    globalName: "JSROOT"
 })(ContentWrapper);
---
+++ src/Components/JSRootGraph.js
@@ -0,0 +1,7 @@
+const JSRootGraph = props=>{
+    const JSROOT = window.JSROOT;
+
+    return(<div>Hello graph</div>)
+}
+
+export default JSRootGraph;
