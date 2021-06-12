a48fd76d68a5d90536465b42d2b01bd7a1a085f0
Mexolius <39342674+Mexolius@users.noreply.github.com>
2021-06-12 15:11:11 +0200

    Preparing helper functions and drawing in div

--- src/Components/JSRootGraph.js
+++ src/Components/JSRootGraph.js
@@ -1,7 +1,36 @@
-const JSRootGraph = props=>{
-    const JSROOT = window.JSROOT;
+import { useRef } from "react";
 
-    return(<div>Hello graph</div>)
+let JSROOT
+//#region Helper functions
+
+function createTGraphFromTrace(trace) {
+    return JSROOT.createTGraph(trace.y.length,trace.x,trace.y)
+}
+
+function createMultigraphFromProps(traces) {
+    return JSROOT.createTMultiGraph(...(traces.map(createTGraphFromTrace)));
+}
+
+//#endregion Helper functions
+
+// COMPONENT
+
+const JSRootGraph = props => {
+    JSROOT = window.JSROOT;
+
+    const ref = useRef(null);
+
+    if(props.traces.length !== 0){
+        const toDraw = createMultigraphFromProps(props.traces);
+        JSROOT.draw(ref.current,toDraw,"",_=>console.log("drawing complete"))
+    }
+
+    return (
+        <div>
+            Hello graph
+            <div ref={ref}></div>
+        </div>
+    )
 }
 
 export default JSRootGraph;
