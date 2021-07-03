dea745e68d17cc14c2ec2eb988c9c2b9aea2ecf2
Mexolius <39342674+Mexolius@users.noreply.github.com>
2021-07-03 13:31:33 +0200

    Fixing react issues

--- src/Components/ContentWrapper.js
+++ src/Components/ContentWrapper.js
@@ -1,4 +1,4 @@
-import { Component } from "react";
+import React,{ Component } from "react";
 import makeAsyncScriptLoader from "react-async-script";
 import Form from "./Form";
 import JSRootGraph from "./JSRootGraph";
