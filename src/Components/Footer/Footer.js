0cb3572c15da0d6960cb562536c5c5859f0fbd7b
Mexolius <39342674+Mexolius@users.noreply.github.com>
2021-08-22 14:59:55 +0200

    Adding nav+footer with react side of version monitoring

---
+++ src/Components/Footer/Footer.js
@@ -0,0 +1,45 @@
+import { useEffect, useState } from "react";
+import logo from './github-logo.png'
+
+import '../../Styles/Footer.css'
+
+
+const Footer = _ => {
+
+    const [info, setInfo] = useState({})
+
+    const getInfo = () => {
+        console.log(window.location)
+        fetch('http://'+window.location.host+'/web_dev/config/deploy.json', {
+            headers: {
+                'Content-Type': 'application/json',
+                'Accept': 'application/json'
+            }
+        })
+            .then(res => {
+                return res.ok && res.json();
+            })
+            .then(json => {
+                setInfo(json);
+            })
+            .catch(err => {
+                console.log(err)
+            })
+
+    }
+
+    useEffect(() => {
+        getInfo();
+    }, [])
+
+
+
+    return (
+        <footer className="footer" id="footer">
+            <div>dEdx-Web &copy; 2021. Code available at <img alt="github-logo" src={logo} /></div>
+            <p>{`commit ${info.commit} released on ${info.date} on brach ${info.branch}`}</p>
+        </footer>
+    );
+}
+
+export default Footer;
---
+++ src/Components/Pages/Energy.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+class EnergyComponent extends React.Component{
+    render(){
+        return <>HELLO ENERGY</>;
+    }
+}
+
+export default EnergyComponent;
