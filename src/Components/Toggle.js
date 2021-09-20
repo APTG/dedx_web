8fdca154f641a57c3314cb5aaae3b19271c7540e
Marek Ślązak <mslazak99@gmail.com>
2021-09-20 12:53:24 +0200

    Adding simple test coverage (#77)
    * Adding partial test coverage for Form.js
    
    * Finishing form tests
    
    * Addig additional tests

--- src/Components/Toggle.js
+++ src/Components/Toggle.js
@@ -1,25 +1,27 @@
 import React, { useState } from 'react';
 import '../Styles/Toggle.css'
 
-const Toggle = ({name, children, onChange, startValue})=>{
-    const [selected, setSelected] = useState(startValue??0);
+const Toggle = ({ name, children, onChange, startValue }) => {
+    const [selected, setSelected] = useState(startValue ?? 0);
 
     const exportChange = (newState) => {
         setSelected(newState);
         onChange(newState);
     }
 
-    return(
+    return (
         <div className="toggle-body">
             {name && <div className="toggle-name">{name}</div>}
             <div className="option-container">
-                {children.map((c,k)=>{
-                return(
-                    <div key={`toggle-${name}-${k}`} onClick={()=>exportChange(k)} className={`toggle-option${selected===k?" selected":""}`}>{c}</div>
-                )
-            })}
+                {children.map((c, k) => {
+                    return (
+                        <div key={`toggle-${name}-${k}`} onClick={() => exportChange(k)} className={`toggle-option${selected === k ? " selected" : ""}`}>
+                            {c}
+                        </div>
+                    )
+                })}
             </div>
-            
+
         </div>
     )
 }
---
+++ src/Components/__test__/Form.test.js
@@ -0,0 +1,71 @@
+import { render, fireEvent } from '@testing-library/react'
+import Form from '../Form'
+import StoppingPowerComponent from '../Pages/StoppingPower'
+
+describe('form', () => {
+    test('should render form', () => {
+        const { getByTestId } = render(<Form onSubmit={() => { }} />)
+
+        expect(getByTestId('form-1')).toBeInTheDocument()
+    })
+
+    test('should render all form fields', () => {
+        const { getByLabelText } = render(<Form onSubmit={() => { }} />)
+        const texts = [
+            'Name',
+            'Plot using',
+            'Material',
+            'Particle',
+        ]
+
+        texts.forEach(t => {
+            expect(getByLabelText(t)).toBeInTheDocument()
+        })
+    })
+
+    test('should be rendered on the stopping power subsite', () => {
+        const { getByLabelText } = render(<StoppingPowerComponent ready={false} />)
+
+        expect(getByLabelText('Name')).toBeInTheDocument()
+    })
+
+    test('should handle text inputs', () => {
+        const { getByLabelText } = render(<StoppingPowerComponent ready={false} />)
+
+        const nameNode = getByLabelText('Name')
+        expect(nameNode.value).toMatch('')
+
+        fireEvent.change(nameNode, { target: { value: 'testName' } })
+        expect(nameNode.value).toMatch('testName')
+    })
+
+    test('should handle numeric input', () => {
+        const { getByLabelText } = render(<StoppingPowerComponent ready={false} />)
+
+        testNumericInput(getByLabelText, 'Plot using')
+    })
+
+    test('should handle submit', () => {
+        const mockfn = jest.fn();
+        const { getByText } = render(<Form onSubmit={mockfn} />)
+
+        fireEvent.click(getByText('Submit'))
+        expect(mockfn).toBeCalledTimes(1)
+        expect(mockfn).toBeCalledWith({
+            name: '',
+            plot_using: '500',
+            method: 0,
+            particle: 'He',
+            material: 'Water',
+        })
+    })
+})
+
+function testNumericInput(queryFunction, name) {
+    const el = queryFunction(name)
+    expect(el.value).not.toBeUndefined()
+
+    const testValue = Math.floor(Math.random() * 1000)
+    fireEvent.change(el, { target: { value: testValue } })
+    expect(el.value).toMatch(testValue + [])
+}
