5a1696ec73c9ad59cec41a1777d5c252f24e50a8
mslazak <marek.slazak@relayr.io>
2021-10-27 21:52:28 +0200

    compilation prototype1

--- build_wasm.sh
+++ build_wasm.sh
@@ -1,24 +1,26 @@
-$PROJECT_NAME = "weblibdedx"
-$WASM_FILENAME = $PROJECT_NAME.wasm
-$WASM_PUBLIC = ../public/$WASM_FILENAME
-$PROPER_PATH = /web_dev/$WASM_FILENAME
-$JS = ../src/Backend/$PROJECT_NAME.js
+PROJECT_NAME=weblibdedx
+WASM_FILENAME=$PROJECT_NAME.wasm
+WASM_PUBLIC=../public/$WASM_FILENAME
+PROPER_PATH="\/web_dev\/$WASM_FILENAME"
+JS=../src/Backend/$PROJECT_NAME.js
+WASM_LOOKUP="wasmBinaryFile = locateFile"
 
-emcmake cmake ../libdedx
+emcmake cmake -S . -B ./build
+
+cd ./build
+
 emmake make -j4
 
-$WASM_LOOKUP = 'wasmBinaryFile = locateFile'
+FUNCTIONS='['
 
-$FUNCTIONS='['
+FUNCTIONS+='_dedx_get_program_list'
 
-$FUNCTIONS += '_dedx_get_program_list'
-
-$FUNCTIONS += ']'
+FUNCTIONS+=']'
 
 emcc libdedx.a -s EXPORTED_FUNCTIONS="$FUNCTIONS" -s ENVIRONMENT='web' -s USE_ES6_IMPORT_META=0 -s EXPORT_ES6=1 -s MODULARIZE=1 -s WASM=1
 
 cp ${WASM_FILENAME} ${WASM_PUBLIC}
 
-sed -i.old '1s;^;\/* eslint-disable *\/;' ${JS}
-sed -i 's|$PROJECT_NAME|$PROPER_PATH|' ${JS}
-sed -i 's|$WASM_LOOKUP|// $WASM_LOOKUP|' ${JS}
+sed -i '1s;^;\/* eslint-disable *\/\n;' ${JS}
+sed -i "s/$WASM_FILENAME/$PROPER_PATH/" ${JS}
+sed -i "s/$WASM_LOOKUP/\/\/$WASM_LOOKUP/" ${JS}
