01743eed9c9aa7ec7fbbf3a395e178e94ba55102
Mexolius <39342674+Mexolius@users.noreply.github.com>
2021-10-28 18:24:37 +0200

    cleanup

--- src/Backend/WASMWrapper.js
+++ src/Backend/WASMWrapper.js
@@ -3,9 +3,21 @@ import Module from './weblibdedx.js'
 let _wasm;
 
 async function _init_wasm(){
+    console.log("initing")
     _wasm = await Module()
 }
 
+// ensures WASM is loaded and the function is compiled and reachable.
+async function getFromWASM(name){
+    if(!_wasm) await _init_wasm()
+    if(_wasm[name]) return _wasm[name]()
+    else return undefined
+}
+
+export async function getPrograms() {
+    if(!_wasm) await _init_wasm()
+    return getFromWASM("_dedx_get_all_programs")
+}
 //#region MOCK
 //#region GET
 export function getTrace(particle, propagationMedium) {
@@ -13,11 +25,6 @@ export function getTrace(particle, propagationMedium) {
         x: [10, 20, 30, 40, 50, 60, 70, 80, 90, 100],
         y: Array.from(new Array(10),_=>Math.random()*100)
     }
-}
-
-export async function getPrograms() {
-    if(!_wasm) await _init_wasm()
-    return _wasm._dedx_get_all_programs()
 }
 
 export function getParticles() {
