f94dfe3090fe8635df3dcc212cb6c26d5434fd26
Mexolius <39342674+Mexolius@users.noreply.github.com>
2021-10-29 19:42:50 +0200

    prototype2

--- build_wasm.sh
+++ build_wasm.sh
@@ -1,8 +1,7 @@
 PROJECT_NAME=weblibdedx
-WASM_FILENAME=$PROJECT_NAME.wasm
-WASM_PUBLIC=../public/$WASM_FILENAME
-PROPER_PATH="\/web_dev\/$WASM_FILENAME"
-JS=../src/Backend/$PROJECT_NAME.js
+WASM_PUBLIC=./public/$PROJECT_NAME.wasm
+PROPER_PATH="\/web_dev\/$PROJECT_NAME.wasm"
+JS=./src/Backend/$PROJECT_NAME.js
 WASM_LOOKUP="wasmBinaryFile = locateFile"
 
 cd ./libdedx
@@ -22,16 +21,17 @@ FUNCTIONS+='_dedx_get_program_list'
 
 FUNCTIONS+=']'
 
-emcc libdedx.a -s EXPORTED_FUNCTIONS="$FUNCTIONS" -s EXPORT_ES6=1 -s MODULARIZE=1 -s WASM=1
+emcc libdedx.a -o $PROJECT_NAME.js -s EXPORTED_FUNCTIONS="$FUNCTIONS" -s EXPORT_ES6=1 -s MODULARIZE=1 -s WASM=1
 
 ls -al
 
-echo -------
+cd ../..
 
-ls -al ..
+cp libdedx/build/libdedx/$PROJECT_NAME.js $JS
+cp libdedx/build/libdedx/WASM_FILENAME $WASM_PUBLIC
 
-# cp ${WASM_FILENAME} ${WASM_PUBLIC}
+rm -d libdedx/build/libdedx/
 
-# sed -i '1s;^;\/* eslint-disable *\/\n;' ${JS}
-# sed -i "s/$WASM_FILENAME/$PROPER_PATH/" ${JS}
-# sed -i "s/$WASM_LOOKUP/\/\/$WASM_LOOKUP/" ${JS}
+sed -i '1s;^;\/* eslint-disable *\/\n;' ${JS}
+sed -i "s/$PROJECT_NAME.wasm/$PROPER_PATH/" ${JS}
+sed -i "s/$WASM_LOOKUP/\/\/$WASM_LOOKUP/" ${JS}
