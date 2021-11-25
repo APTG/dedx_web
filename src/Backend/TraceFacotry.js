29c7b1bd4377d20d31fc4832523db44f900ea54a
mslazak <marek.slazak@relayr.io>
2021-11-25 19:53:18 +0100

    add Tracefactory.js

---
+++ src/Backend/TraceFacotry.js
@@ -0,0 +1,27 @@
+export default class TraceFactory{
+    static getXAxisByPoints(start, end, points){
+        const interval = (end-start)/points
+        return this.getXAxis(start,end, points,interval)
+    }
+
+    static getXAxisByInterval(start,end,interval){
+        const points = Math.ceil((end-start)/interval)
+        return this.getXAxis(start,end,points,interval)
+    }
+
+    static getXAxis(start,end,points,interval){
+        console.log(points)
+        const arr = Array.from(new Array(points-1),(_,key)=>{return (Number)((start + interval * (key+1).toFixed(3)))})
+        arr.push(end)
+        return arr
+    }
+
+    static getStoppingPowerValues(xs, stepFunction){
+        console.log(stepFunction)
+        const ys = xs.map(stepFunction)
+        return {
+            x: xs,
+            y: ys
+        }
+    }
+}
