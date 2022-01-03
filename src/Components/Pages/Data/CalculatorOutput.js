ca1f1668280518e20161d8c9985e6cff2e04ff40
Marek Ślązak <mslazak99@gmail.com>
2022-01-03 20:04:37 +0100

    Rescale csda by density (#177)
    * add density function to wasm
    
    * Fixing the density issue
    
    * update square sign
    
    * Crash fix

--- src/Components/Pages/Data/CalculatorOutput.js
+++ src/Components/Pages/Data/CalculatorOutput.js
@@ -1,12 +1,12 @@
 import ResultTable from "../../ResultTable/ResultTable"
 import { transformResultToTableData } from "../../ResultTable/TableUtils"
 
-function CalculatorOutput({ result, stoppingPowerUnit }) {
+function CalculatorOutput({ result, stoppingPowerUnit, isRangeDensityBased }) {
     const { energies } = result
     return (
         <ResultTable
             energies={energies}
-            values={transformResultToTableData(result, stoppingPowerUnit)}
+            values={transformResultToTableData(result, stoppingPowerUnit, isRangeDensityBased)}
             shouldDisplay={energies && energies?.length !== 0}
         />
     )
