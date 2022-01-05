638996262d66bd933face9f7e8b5f3f028a6acbe
mslazak <marek.slazak@relayr.io>
2022-01-05 16:04:29 +0100

    progress

--- build_wasm.sh
+++ build_wasm.sh
@@ -1,6 +1,5 @@
 PROJECT_NAME=weblibdedx
-PROPER_PATH="\`\${process.env.PUBLIC_URL}\/$PROJECT_NAME.wasm\`"
-BROKEN_PATH="wasmBinaryFile = \'weblibdedx.wasm\'"
+FIXED_PATH="\${process.env.PUBLIC_URL}\/$PROJECT_NAME.wasm"
 JS=./src/Backend/$PROJECT_NAME.js
 WASM_LOOKUP="wasmBinaryFile = locateFile"
 
@@ -58,7 +57,7 @@ cp ./libdedx/build/libdedx/$PROJECT_NAME.js ./src/Backend
 cp ./libdedx/build/libdedx/$PROJECT_NAME.wasm ./public
 
 sed -i '1s;^;\/* eslint-disable *\/\n;' ${JS}
-sed -i "s/$BROKEN_PATH/$PROPER_PATH/" ${JS}
+sed -i "s/'$PROJECT_NAME.wasm'/\`$FIXED_PATH\`/g" ${JS}
 sed -i "s/$WASM_LOOKUP/\/\/$WASM_LOOKUP/" ${JS}
 
 #cleanup
