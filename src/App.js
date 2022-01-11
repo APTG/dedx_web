8570e1a77a03dcb1f8780967d1a781b53ed3129c
Mexolius <39342674+Mexolius@users.noreply.github.com>
2022-01-11 17:22:30 +0100

    title and favicon

--- src/App.js
+++ src/App.js
@@ -4,10 +4,6 @@ import './App.css';
 import Main from './Components/Main';
 
 function App() {
-  useEffect(() => {
-    document.title = "libdEdx web"
-  }, []);
-
   return (
     <div>
       <Main />
